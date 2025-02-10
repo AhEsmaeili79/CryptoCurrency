ffc2706f152b2ee79e2f477d26121ae8a7762787
Amirhossein Esmaeili <ah.esmaeili.79@gmail.com>
2025-02-11 01:25:53 +0330

    update : update env

--- frontend/src/api/transactionApi.ts
+++ frontend/src/api/transactionApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_API_URL;
+const API_URL = import.meta.env.VITE_BACKEND_API;
 
 const token = localStorage.getItem("access_token");
 
